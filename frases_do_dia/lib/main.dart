0a0bb0fe09775461ace762b68be6bdf1b30c5b56
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-03-22 19:01:22 -0300

    utilizando columns
    Exemplo de utilização de columns
    removido test pois não estava compilando completamente pelo fato de ter ele

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    title: "Frases do dia",
-    home: Container(
-      color: Colors.white,
-    ),
-  )
-);
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Frases do dia",
+      home: Column(
+        children: <Widget>[
+          Text("t1:"),
+          Text("t2:"),
+          Text("t3:"),
+        ],
+      )
+    )
+  );
+}
