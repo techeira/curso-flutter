4d5122167c40e3f57b97ae4bb5b2dce94467f076
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-03-22 20:12:48 -0300

    formatação de textos

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,12 +4,27 @@ void main(){
   runApp(
     MaterialApp(
       title: "Frases do dia",
-      home: Row(
-        children: <Widget>[
-          Text("t1:"),
-          Text("t2:"),
-          Text("t3:"),
-        ],
+      home: Container(
+				color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Text(
+							//"aí vai um textão só pra testar a quantidade de caracteres",
+              "teste teste",
+							style: TextStyle(
+								fontSize: 40,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.normal,
+								color: Colors.black,
+                letterSpacing: 0,
+                wordSpacing: 0,
+                decoration: TextDecoration.none,
+                decorationColor: Colors.black,
+                decorationStyle: TextDecorationStyle.solid
+							),
+						),
+          ],
+        ),
       )
     )
   );
