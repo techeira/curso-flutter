dc41ffe3124f2d6d7982da3414e6290b1afec513
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-03-23 00:59:11 -0300

    exemplo stateful e statless

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,44 +1,104 @@
 import 'package:flutter/material.dart';
 
+/*
+Stateless -> Widgets que não podem ser alterados
+Stateful -> Widgets que podem ser alterados
+*/
+
 void main(){
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false, //remove a "bandeira de debug"
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Frases do dia"),
-          backgroundColor: Color(0xFF656565),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.blue,
-              style: BorderStyle.solid
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Text(
-              "body",
-            ),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Color(0xFFC6C6C6),
-          child: Container(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text("texto 1"),
-                  Text("texto 2"),
-                ],
-              )
-            ),
-          ),
-        ),
-      )
+      home: HomeStateful(),
+      //home: Home(),
     )
   );
+}
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+
+  var _titulo = "Frases do dia";
+  var _nome = "Fulano";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulo),
+        backgroundColor: Color(0xFF656565),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blue,
+            style: BorderStyle.solid
+          ),
+        ),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  _nome="Ciclano";
+                });
+              },
+              child: Text(
+                "Clique aqui"
+              ),
+            ),
+            Text("nome: $_nome")
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+
+  var _titulo = "Frases do dia";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulo),
+        backgroundColor: Color(0xFF656565),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blue,
+            style: BorderStyle.solid
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            "body",
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color(0xFFC6C6C6),
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text("texto 1"),
+                Text("texto 2"),
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
 }
