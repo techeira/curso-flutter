5ada579841902c65b263911eb1ec0aaec0f7bf76
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-03-22 22:05:30 -0300

    Exemplos Margin e Padding

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,24 +3,50 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
+      //debugShowCheckedModeBanner: false, //remove a "bandeira de debug"
       title: "Frases do dia",
       home: Container(
-				color: Colors.white,
+				//color: Colors.black,
+        padding: EdgeInsets.fromLTRB(
+          0,
+          30,
+          0,
+          30,
+        ),
+        margin: EdgeInsets.fromLTRB(
+          10,
+          30,
+          10,
+          30
+        ),
+        decoration: BoxDecoration(
+          //color: Colors.white,
+          border: Border.all(
+            color: Colors.cyan,
+            style: BorderStyle.solid,
+            width: 1,
+          )
+        ),
         child: Column(
           children: <Widget>[
-            FlatButton(
-              onPressed: (){
-                print("Botão pressionado");
-              },
+            Text(
+              "t1",
+            ),
+            Padding(
+              padding: EdgeInsets.all(30),
               child: Text(
-                "Botão",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  decoration: TextDecoration.none
-                ),
-              ),
+                "t2"
+              )
+            ),
+            Text(
+              "t3",
             )
+            /*
+            Text(
+              "Imagine aqui um texto bonito que vou usar apenas para testar",
+              textAlign: TextAlign.justify,
+            )
+            */
           ],
         ),
       )
