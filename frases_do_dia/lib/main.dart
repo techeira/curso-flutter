669e53368a78b562fb8d7c031312e86dc811c0c1
BRUNO H. TECHEIRA <bruno.bht.bh@gmail.com>
2020-03-22 23:58:00 -0300

    exemplo com scaffold

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
-
-
 void main(){
   runApp(
     MaterialApp(
-      //debugShowCheckedModeBanner: false, //remove a "bandeira de debug"
-      title: "Frases do dia",
-      home: Container(
-        margin: EdgeInsets.only(
-          top: 40,
+      debugShowCheckedModeBanner: false, //remove a "bandeira de debug"
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Frases do dia"),
+          backgroundColor: Color(0xFF656565),
         ),
-        decoration: BoxDecoration(
-          //color: Colors.white,
-          border: Border.all(
-            color: Colors.cyan,
-            style: BorderStyle.solid,
-            width: 1,
-          )
+        body: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.blue,
+              style: BorderStyle.solid
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              "body",
+            ),
+          ),
         ),
-        child: Image.asset(
-          "images/mesa.jpg",
-          fit: BoxFit.contain,
-        )
+        bottomNavigationBar: BottomAppBar(
+          color: Color(0xFFC6C6C6),
+          child: Container(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text("texto 1"),
+                  Text("texto 2"),
+                ],
+              )
+            ),
+          ),
+        ),
       )
     )
   );
